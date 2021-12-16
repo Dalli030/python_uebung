b5d0d2ec715e6c5c8da4c2329917430a1a324d29
Dalli75 <Dallikaes+gh@gmail.com>
2021-12-16 15:10:53 +0100

    unused titlewidth len() check removed

--- PythonUebung1.py
+++ PythonUebung1.py
@@ -2,16 +2,13 @@
 # -*- coding: iso-8859-1 -*-
 
 linesymbol   = '-'                  # Zeichen für die Menu Linie
-greetingtext = 'Moin moin'          # Begrüßungstext
+greetingtext = 'MOIN MOIN'          # Begrüßungstext
 askname      = 'Wie heißt du? '     # Frage nach Name
 
-err_tittolong = "Der Titel darf nicht länger als Titelline sein (",width,") sein" 
 width         = len(greetingtext)*2 # Wie breit das Menu wird
 
 line = linesymbol * width 
-if len(greetingtext)>width:
-    print(err_tittolong) 
-    quit()
+
 print(line)
 print(greetingtext.center(width," "))
 print(line)
