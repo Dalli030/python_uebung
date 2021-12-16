e156814e084be73d30f2a0f9b8e31bca19c50474
Dalli75 <Dallikaes+gh@gmail.com>
2021-12-16 14:57:21 +0100

    auto genberate the title lines

--- PythonUebung1.py
+++ PythonUebung1.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: iso-8859-1 -*-
 
-line        = '------------------------------'
-greetings   = '          MOIN MOIN           '
-askname     = 'Wie heißt du? '
+linesymbol   = '-'                  # Zeichen für die Menu Linie
+width        = 30                   # Wie breit das Menu wird
+greetingtext = 'Moin moin'          # Begrüßungstext
+askname      = 'Wie heißt du? '     # Frage nach Name
 
+err_tittolong = "Der Titel darf nicht länger als Titelline sein (",width,") sein" 
+
+line = linesymbol * width 
+if len(greetingtext)>width:
+    print(err_tittolong) 
+    quit()
 print(line)
-print(greetings)
+print(greetingtext.center(width," "))
 print(line)
 name = input(askname)
 print('\nMoin moin,',name)
