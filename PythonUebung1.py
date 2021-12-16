aa1c4b2d40a57c738c85ef335ad32949a9525264
Dalli75 <Dallikaes+gh@gmail.com>
2021-12-16 14:27:57 +0100

    nit

---
+++ PythonUebung1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: iso-8859-1 -*-
+
+line        = '------------------------------'
+greetings   = '          MOIN MOIN           '
+askname     = 'Wie heiﬂt du? '
+
+print(line)
+print(greetings)
+print(line)
+name = input(askname)
+print('\nMoin moin,',name)
